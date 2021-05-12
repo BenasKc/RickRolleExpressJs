90a0cf2308c49f055fc5ea1fe0a9de23c6e8ac2a
Benas <68812359+BenasKc@users.noreply.github.com>
2021-05-12 12:15:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
+ 
+'use strict';
 const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
 const app = express();
-app.get('/',function(req,res){res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');})
+const bodyParser = require('body-parser');
+
 app.listen(process.env.port || 3001);
-console.log('Done');
+
+
+const router = express.Router();
+router.get('/', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Get Ricked Rolled!</h1>');
+  res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  res.end();
+});
+router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
+router.post('/', (req, res) => res.json({ postBody: req.body }));
+
+app.use(bodyParser.json());
+app.use('/.netlify/functions/server', router);
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+module.exports = app;
+module.exports.handler = serverless(app);
