d34c3f3c00a7fb96f0896232bace70843f3c1753
Benas <68812359+BenasKc@users.noreply.github.com>
2021-05-12 13:05:13 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -7,8 +7,6 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
   res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
   res.end();
 });
