d0116a4188e494a68f87a871c994a943eab73266
Benas <68812359+BenasKc@users.noreply.github.com>
2021-05-12 13:12:37 +0300

    Update server.js

--- express/server.js
+++ express/server.js
@@ -8,14 +8,13 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-  res.end();
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ'););
+app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
